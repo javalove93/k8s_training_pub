7187a733984bf39f171b139f91d63fdc6aad1d66
Jerry Jeong <javalove93@gmail.com>
2024-09-19 00:13:04 +0900

    copy k8s scripts

--- scrips/04.install_docker.sh
+++ scrips/04.install_docker.sh
@@ -9,3 +9,6 @@ echo   "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.asc] https://download
 sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
