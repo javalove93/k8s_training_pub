ea739bac927aa1a4d188a85d5b2aa3843f3e7f7d
Jerry Jeong <javalove93@gmail.com>
2024-09-19 00:14:40 +0900

    copy k8s scripts

--- scrips/04.install_docker.sh
+++ scrips/04.install_docker.sh
@@ -8,7 +8,9 @@ sudo chmod a+r /etc/apt/keyrings/docker.asc
 echo   "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian   bookworm stable" |   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo systemctl enable docker
 sudo usermod -aG docker $USER
+
+echo SSH 세션 재접속 후 docker 명령어를 사용할 수 있습니다.
